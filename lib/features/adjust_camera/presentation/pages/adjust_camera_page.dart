0e349c16a379cf1aa46d478c9235676804ddb3f6
Ahmed Galal <iahmedgalal@gmail.com>
2025-05-03 22:01:14 +0300

    commit camera

--- lib/features/adjust_camera/presentation/pages/adjust_camera_page.dart
+++ lib/features/adjust_camera/presentation/pages/adjust_camera_page.dart
@@ -26,7 +26,7 @@ class _AdjustCameraPageState extends State<AdjustCameraPage> {
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) async {
-      // initCamera();
+      initCamera();
     });
   }
 
